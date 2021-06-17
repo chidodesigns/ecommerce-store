57c8c2f6c00f24e3961f9aad8aedcbc164c8c3ce
chidoukaigwe <chidodesigns@gmail.com>
2021-06-17 14:51:33 +0100

    update order to paid endpoint created

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -56,7 +56,36 @@ const getOrderById = asyncHandler(async (req, res) => {
 
 });
 
+/**
+ * @desc    Update order to paid
+ * @route   PUT /api/orders/:id
+ * @access  Private
+ */
+const updateOrderToPaid = asyncHandler(async (req, res) => {
+
+  const order = await Order.findById(req.params.id)
+  if(order) {
+    order.isPaid = true
+    order.paidAt = Date.now()
+    //  Data For Gateway Merchant ex:Paypal
+    //  Specific Obj Props come from Paypal
+    order.paymentResult = {
+      id: req.body.id,
+      status: req.body.status,
+      update_time: req.body.update_time,
+      email_address: req.body.payer.email_address
+    }
+    const updatedOrder = await order.save()
+    res.json(updatedOrder)
+  }else{
+    res.status(404)
+    throw new Error('Order not found')
+  }
+
+});
+
 export {
     addOrderItems,
-    getOrderById
+    getOrderById,
+    updateOrderToPaid
 };
