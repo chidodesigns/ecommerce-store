71273643e25bd6394401f3e580cfcd9d66c964dd
chidodesigns <chidoukaigwe@gmail.com>
2021-12-31 02:03:55 +0000

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express  from 'express';
 import dotenv  from 'dotenv';
+dotenv.config();
 import colors from 'colors'
 import connectDB from './config/db.js';
 import morgan from 'morgan'
@@ -15,7 +16,7 @@ import orderRoutes from './routes/orderRoutes.js'
 import uploadRoutes from './routes/uploadRoutes.js'
 import thirdPartyApiRoutes from './routes/thirdPartyApiRoutes.js'
 
-dotenv.config();
+
 
 connectDB();
 
@@ -55,6 +56,6 @@ if(process.env.NODE_ENV === "production"){
 app.use(notFound)
 app.use(errorHandler)
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 8000
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
--- frontend/src/components/layout/Header.js
+++ frontend/src/components/layout/Header.js
@@ -22,7 +22,7 @@ const Header = () => {
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
         <LinkContainer to="/">
-        <Navbar.Brand >EarnMoreCrpyto</Navbar.Brand>
+        <Navbar.Brand >The Wise Fool</Navbar.Brand>
         </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
